1dd8f0acd4573a804cb897c72952d0bfcf2e997d
Lipitt <victor.david.lip@gmail.com>
2021-09-03 18:48:12 -0300

    feat/issue-17 added BottomNav component to index.js

--- pages/index.js
+++ pages/index.js
@@ -1,32 +1,36 @@
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
 import { motion } from 'framer-motion';
+import BottomNav from '../components/BottomNav';
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>CMYK - Lime</title>
-        <meta name="description" content="Interactive children app" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    <div>
+      <div className={styles.container}>
+        <Head>
+          <title>CMYK - Lime</title>
+          <meta name="description" content="Interactive children app" />
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
 
-      <main className={styles.main}>
-        <motion.div
-          drag
-          dragConstraints={{
-            top: -50,
-            left: -50,
-            right: 50,
-            bottom: 50,
-          }}
-          whileHover={{ scale: 1.3 }}
-        >
-          <h1 className={styles.title}>
-            Welcome Team <span className={styles.lime}>Lime</span>!
-          </h1>
-        </motion.div>
-      </main>
+        <main className={styles.main}>
+          <motion.div
+            drag
+            dragConstraints={{
+              top: -50,
+              left: -50,
+              right: 50,
+              bottom: 50,
+            }}
+            whileHover={{ scale: 1.3 }}
+          >
+            <h1 className={styles.title}>
+              Welcome Team <span className={styles.lime}>Lime</span>!
+            </h1>
+          </motion.div>
+        </main>
+      </div>
+      <BottomNav />
     </div>
   );
 }
