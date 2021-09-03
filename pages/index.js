b086ece8df8085b54cb23158e7f3a843ab007d14
Lipitt <victor.david.lip@gmail.com>
2021-09-03 19:08:28 -0300

    feat/issue-17: added comments regarding problems

--- pages/index.js
+++ pages/index.js
@@ -5,31 +5,29 @@ import BottomNav from '../components/BottomNav';
 
 export default function Home() {
   return (
-    <div>
-      <div className={styles.container}>
-        <Head>
-          <title>CMYK - Lime</title>
-          <meta name="description" content="Interactive children app" />
-          <link rel="icon" href="/favicon.ico" />
-        </Head>
+    <div className={styles.container}>
+      <Head>
+        <title>CMYK - Lime</title>
+        <meta name="description" content="Interactive children app" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
 
-        <main className={styles.main}>
-          <motion.div
-            drag
-            dragConstraints={{
-              top: -50,
-              left: -50,
-              right: 50,
-              bottom: 50,
-            }}
-            whileHover={{ scale: 1.3 }}
-          >
-            <h1 className={styles.title}>
-              Welcome Team <span className={styles.lime}>Lime</span>!
-            </h1>
-          </motion.div>
-        </main>
-      </div>
+      <main className={styles.main}>
+        <motion.div
+          drag
+          dragConstraints={{
+            top: -50,
+            left: -50,
+            right: 50,
+            bottom: 50,
+          }}
+          whileHover={{ scale: 1.3 }}
+        >
+          <h1 className={styles.title}>
+            Welcome Team <span className={styles.lime}>Lime</span>!
+          </h1>
+        </motion.div>
+      </main>
       <BottomNav />
     </div>
   );
