a729258fea5b200e6033078369ed816164c57f0f
AlejandroEB89 <alejandroberrini@hotmail.com>
2021-09-04 12:09:53 -0300

    feat/Issue-16: Agrega About Page

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,8 @@ import Head from 'next/head';
 import styles from '../styles/Home.module.css';
 import { motion } from 'framer-motion';
 
+
+
 export default function Home() {
   return (
     <div className={styles.container}>
