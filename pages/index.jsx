df7c3dd0c2e488280fc494f6c98f734eae0e3462
JoniMerce <jonatanmerce@gmail.com>
2021-09-03 23:28:03 -0300

    se agrego svg circle y el style

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,6 +1,7 @@
-import Head from 'next/head';
-import styles from '../styles/Home.module.css';
 import { motion } from 'framer-motion';
+import Head from 'next/head';
+
+import styles from '../styles/Home.module.css';
 
 export default function Home() {
   return (
---
+++ pages/punto/07/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { Carita } from './carita';
+
+const punto_07 = () => {
+  return (
+    <div>
+      <Carita></Carita>
+    </div>
+  );
+};
+
+export default punto_07;
