f59ca24a27359d4a0b24c72fd3ba1980fe97a06d
Lipitt <victor.david.lip@gmail.com>
2021-09-06 13:15:48 -0300

    feat/issue 17: removed second link tag & passHref from BottomNavButton component. added anchor tag

--- components/BottomNavButton.js
+++ components/BottomNavButton.js
@@ -5,13 +5,11 @@ import styles from '../styles/BottomNavButton.module.css';
 const BottomNavButton = ({ text, route, icon }) => {
   return (
     <div className={styles.button}>
-      <div>
-        <Link href={route} passHref>
+      <Link href={route}>
+        <a>
           <Image alt="" src={icon}></Image>
-        </Link>
-      </div>
-      <Link href={route} passHref>
-        <h1 className={styles.text}>{text}</h1>
+          <h1 className={styles.text}>{text}</h1>
+        </a>
       </Link>
     </div>
   );
