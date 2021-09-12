989fa61edd1fc2169cfe9450e999125b40f23ccc
Lipitt <victor.david.lip@gmail.com>
2021-09-12 12:02:03 -0300

    issue-20: created Circle.js component svg

--- pages/punto/05.js
+++ pages/punto/05.js
@@ -3,7 +3,9 @@ import styles from '../../styles/Home.module.css';
 export default function Page5() {
   return (
     <div className={styles.container}>
-      <main className={styles.main}></main>
+      <main className={styles.main}>
+        <Circle />
+      </main>
     </div>
   );
 }
