c7ca1c108cb434d8c1b3113bba7a0f0342f3286a
Lipitt <victor.david.lip@gmail.com>
2021-09-12 11:58:20 -0300

    issue-20: created folders and files needed

---
+++ pages/punto/05.js
@@ -0,0 +1,9 @@
+import styles from '../../styles/Home.module.css';
+
+export default function Page5() {
+  return (
+    <div className={styles.container}>
+      <main className={styles.main}></main>
+    </div>
+  );
+}
