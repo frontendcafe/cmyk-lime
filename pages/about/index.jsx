2cfc0d8deac174cafc01efdcd5341f74c3691141
AlejandroEB89 <alejandroberrini@hotmail.com>
2021-09-13 18:14:55 -0300

    fix/issue-16: Corrije css y crea componentes para miembros

--- pages/about/index.jsx
+++ pages/about/index.jsx
@@ -1,10 +1,37 @@
-import React, {Component} from "react";
-import styles from '../styles/About.module.css'
+import styles from '../../styles/About.module.css';
+import Members from './Members';
+import Team from './Team';
+
+const About = () => {
+  return (
+    <main className={styles.background}>
+      <div className={styles.container}>
+        <h3 className={styles.title}>Acerca de esta App...</h3>
+        <h1 className={styles.lime}>CMYK 4: GRUPO LIME</h1>
+        <p className={styles.negrita}>
+          Idea incial: Juego narrativo con arte en base a texto
+        </p>
+        <div className={styles.textContainer}>
+          <Members rol="Coordinador" name="Maximiliano Blanco" />
+          <Members rol="Diseñador" name="Mauro Lazzarino" />
+          <Members rol="Referente" name="Alan Pérez" />
+        </div>
+        <p className={styles.text}>Equipo</p>
+        <div className={styles.textContainer}>
+          <Team name1="Antonella Cardozo" name2="Jonatan Merce" />
+          <Team name1="David Lipshitz" name2="Alejandro Berrini" />
+          <Team name1="Karen Duarte" name2="Rortan" />
+        </div>
+      </div>
+    </main>
+  );
+};
+
+/*
 
 const About = () =>{
     return (
-        <body className={styles.background}>
-            <articulo>
+        <main className={styles.background}>
                 <div className={styles.container}>
                     <h3 className={styles.title}>Acerca de esta App...</h3>
                     <h1 className={styles.lime}>CMYK 4: GRUPO LIME</h1>
@@ -42,10 +69,9 @@ const About = () =>{
                         </div>
                     </div>
                 </div>
-            </articulo>
-        </body>
+        </main>
         
     );    
-}
+}*/
 
 export default About;
