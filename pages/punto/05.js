6f9f2a69a4551e797217ac91bc2dfc40014592e0
Lipitt <victor.david.lip@gmail.com>
2021-09-18 19:19:51 -0300

    feat/issue-20: added slashed circles & their animation

--- pages/punto/05.js
+++ pages/punto/05.js
@@ -1,11 +1,61 @@
+import Head from 'next/head';
 import styles from '../../styles/Home.module.css';
-import Circle from '../../components/Circle';
+import { motion } from 'framer-motion';
+import PuntoPuppet from '../../components/PuntoPuppet';
 
-export default function Page5() {
+export default function Page05() {
+  const circleStyle = {
+    border: '2px dashed #cccccc',
+    borderRadius: '50%',
+    opacity: 1,
+    width: '100px',
+    height: '100px',
+    position: 'absolute',
+  };
   return (
     <div className={styles.container}>
+      <Head>
+        <title>CMYK - Lime</title>
+        <meta name="description" content="Interactive children app" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
       <main className={styles.main}>
-        <Circle />
+        <motion.div
+          drag
+          whileHover={{ scale: 1.1 }}
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ delay: 5, duration: 1.5 }}
+        >
+          {/* Prueben cambiar de happy a blushed o a surprised y traten que abra los ojos */}
+          {/* Pasar una prop asi es equivalente a happy={true} */}
+          <motion.div
+            style={circleStyle}
+            animate={{ x: 100, y: -100 }}
+            initial={{ rotate: 1 }}
+            transition={{
+              delay: 7,
+              ease: 'linear',
+              duration: 1,
+              repeat: Infinity,
+              repeatDelay: 5,
+            }}
+          ></motion.div>
+          <motion.div
+            style={circleStyle}
+            animate={{ x: -100, y: 100 }}
+            initial={{ rotate: 1 }}
+            transition={{
+              delay: 7,
+              ease: 'linear',
+              duration: 1,
+              repeat: Infinity,
+              repeatDelay: 5,
+            }}
+          ></motion.div>
+          <PuntoPuppet eyesOpen />
+        </motion.div>
       </main>
     </div>
   );
